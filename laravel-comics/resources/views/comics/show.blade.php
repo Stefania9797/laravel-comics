91409843702108d81617c76ee2d32cb846999cbb
Stefania9797 <stefaniamastrel@gmail.com>
2021-07-09 17:45:19 +0200

    completato design main di pagine singole

--- laravel-comics/resources/views/comics/show.blade.php
+++ laravel-comics/resources/views/comics/show.blade.php
@@ -19,7 +19,7 @@
                 <div class="d-flex">
                     <!-- prezzo -->
                     <div class="price-info">
-                        <p><span>U.S. Price</span> {{$comic['price']}}</p>
+                        <p><span>U.S. Price:</span> {{$comic['price']}}</p>
                     </div>
                     <div class="available-info">
                         <h6>AVAILABLE</h6>
@@ -36,50 +36,81 @@
         </div>
         <div class="right-content">
             <!-- pubblicità -->
-            <img src="{{asset('images/spot.jpg')}}" alt="">
-        </div>
-    </div>
-    <div class="bottom-info">
-        <!-- talent info -->
-        <div class="talent">
-            <h2>talent</h2>
-            <!-- art by -->
-            <div class="art-by">
-                <h5>art by</h5>
-                <!-- artists -->
-                <span>
-                     @foreach($comic['artists'] as $artist)
-                        <a href="#">{{$artist}}</a>
-                    @endforeach
-                </span>
-            </div> 
-            <!-- written by -->
-            <div class="written-by">
-                <h5>written by</h5>
-                <!-- writers -->
-                <span>
-                     @foreach($comic['writers'] as $writer)
-                        <a href="#">{{$writer}}</a>
-                    @endforeach
-                </span>
-            </div>   
-        </div>
-        <!-- specs info -->
-        <div class="specs">
-            <h2>specs</h2>
-            <div class="series">
-                <h5>series:</h5>
-                <span>{{$comic['series']}}</span>
-            </div>
-            <div class="us-price">
-                <h5>U.S. Price:</h5>
-                <span>{{$comic['price']}}</span>
-            </div>
-            <div class="on-sale">
-                <h5>On-sale date:</h5>
-                <span>{{$comic['sale_date']}}</span>
+            <div class="adv">
+                <h4>Advertisment</h4>
+                <img src="{{asset('images/spot.jpg')}}" alt="">
             </div>
         </div>
     </div>
 </div>
+    <div class="bottom-info">
+        <div class="small-container">
+            <!-- talent info -->
+            <div class="talent">
+                <h2>Talent</h2>
+                <!-- art by -->
+                <div class="art-by">
+                    <div class="art-by-title">
+                        <h5>art by</h5>
+                    </div>
+                    <!-- artists -->
+                    <div class="artists">
+                        <span>
+                            @foreach($comic['artists'] as $artist)
+                            <a href="#">{{$artist}}</a>
+                            @if(!$loop->last)
+                            ,
+                            @endif
+                            @endforeach
+                        </span>
+                    </div>
+                </div> 
+                <!-- written by -->
+                <div class="written-by">
+                    <div class="written-by-title">
+                        <h5>written by</h5>
+                    </div>
+                    <!-- writers -->
+                    <div class="writers">
+                        <span>
+                            @foreach($comic['writers'] as $writer)
+                            <a href="#">{{$writer}}</a>
+                            @if(!$loop->last)
+                            ,
+                            @endif
+                            @endforeach
+                        </span>
+                    </div>
+                </div>   
+            </div>
+            <!-- specs info -->
+            <div class="specs">
+                <h2>Specs</h2>
+                <div class="series">
+                    <div class="series-title">
+                        <h5>series:</h5>
+                    </div>
+                    <div class="series-info">
+                        <a href="">{{$comic['series']}}</a>
+                    </div>
+                </div>
+                <div class="us-price">
+                    <div class="us-price-title">
+                        <h5>U.S. Price:</h5>
+                    </div>
+                    <div class="us-price-info">
+                        <span>{{$comic['price']}}</span>
+                    </div>
+                </div>
+                <div class="on-sale">
+                    <div class="onsale-title">
+                        <h5>On-sale date:</h5>
+                    </div>
+                    <div class="onsale-info">
+                        <span>{{$comic['sale_date']}}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
