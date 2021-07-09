9458a740287bf3c3b85fdc9f15497bc9aa8daa81
Stefania9797 <stefaniamastrel@gmail.com>
2021-07-09 16:01:38 +0200

    stampate info (design da sistemare)

--- laravel-comics/resources/views/comics/index.blade.php
+++ laravel-comics/resources/views/comics/index.blade.php
@@ -3,62 +3,64 @@
 @section('title', 'Comics')
 
 @section('content')
-<div class="container">
-    <h3>Current series</h3>
-    <div class="comics">
-        @foreach ($comics as $index=> $comic)
-        <div class="comic">
-            <a href="{{route('comic' , ['id'=> $index])}}">
-                <div class="cover" style="background-image: url('{{$comic['thumb']}}');"></div>
-                <h6>{{$comic["series"]}}</h6>
-            </a>
-        </div>
-        @endforeach
-        <div class="load-more">
-            <a href="">load more</a>
-        </div>
-    </div>
-</div>
-<div class="info">
-    <div class="container">
-        <div class="info-container">
-            <div class="info-logo">
-            <img src="{{asset('images/buy-comics-digital-comics.png')}}" alt="">
-            </div>
-            <div class="info-text">
-            <h5>Digital comics</h5>
+<div class="home-content">
+        <div class="container">
+            <h3>Current series</h3>
+            <div class="comics">
+                @foreach ($comics as $index=> $comic)
+                <div class="comic">
+                    <a href="{{route('comic' , ['id'=> $index])}}">
+                        <div class="cover" style="background-image: url('{{$comic['thumb']}}');"></div>
+                        <h6>{{$comic["series"]}}</h6>
+                    </a>
+                </div>
+                @endforeach
+                <div class="load-more">
+                    <a href="">load more</a>
+                </div>
             </div>
         </div>
-        <div class="info-container">
-            <div class="info-logo">
-            <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="">
+        <div class="info">
+            <div class="container">
+                <div class="info-container">
+                    <div class="info-logo">
+                        <img src="{{asset('images/buy-comics-digital-comics.png')}}" alt="">
+                    </div>
+                    <div class="info-text">
+                        <h5>Digital comics</h5>
+                    </div>
+                </div>
+                <div class="info-container">
+                    <div class="info-logo">
+                        <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="">
+                    </div>
+                    <div class="info-text">
+                        <h5>DC Merchandise</h5>
+                    </div>
+                </div>
+                <div class="info-container">
+                    <div class="info-logo">
+                        <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="">
+                    </div>
+                    <div class="info-text">
+                        <h5>Subscription</h5>
+                    </div>
             </div>
-            <div class="info-text">
-            <h5>DC Merchandise</h5>
+            <div class="info-container">
+                <div class="info-logo">
+                    <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="">
+                </div>
+                <div class="info-text">
+                    <h5>comic shop locator</h5>
+                </div>
             </div>
-        </div>
-        <div class="info-container">
-            <div class="info-logo">
-            <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="">
-            </div>
-            <div class="info-text">
-            <h5>Subscription</h5>
-            </div>
-        </div>
-        <div class="info-container">
-            <div class="info-logo">
-            <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="">
-            </div>
-            <div class="info-text">
-            <h5>comic shop locator</h5>
-            </div>
-        </div>
-        <div class="info-container">
-            <div class="info-logo">
-            <img src="{{asset('images/buy-dc-power-visa.svg')}}" alt="">
-            </div>
-            <div class="info-text">
-            <h5>dc power visa</h5>
+            <div class="info-container">
+                <div class="info-logo">
+                    <img src="{{asset('images/buy-dc-power-visa.svg')}}" alt="">
+                </div>
+                <div class="info-text">
+                    <h5>dc power visa</h5>
+                </div>
             </div>
         </div>
     </div>
--- laravel-comics/resources/views/comics/show.blade.php
+++ laravel-comics/resources/views/comics/show.blade.php
@@ -3,5 +3,83 @@
 @section('title', 'Comics')
 
 @section('content')
-<h1>Ciao</h1>
+<div class="divider"></div>
+<div class="small-container">
+    <div class="top-content">
+        <div class="left-content">
+            <div class="comic-cover">
+                <img src="{{$comic['thumb']}}" alt="">
+            </div>
+            <!-- titolo fumetto -->
+            <div class="title">
+                <h1>{{$comic['title']}}</h1>
+            </div>
+            <!-- info disponibilità e prezzo -->
+            <div class="green-info">
+                <div class="d-flex">
+                    <!-- prezzo -->
+                    <div class="price-info">
+                        <p><span>U.S. Price</span> {{$comic['price']}}</p>
+                    </div>
+                    <div class="available-info">
+                        <h6>AVAILABLE</h6>
+                    </div>
+                </div>
+                <!-- controllo disponibilità -->
+                <div class="availabilty">
+                    <p>Check availability</p>
+                </div>
+            </div>
+            <div class="description">
+                <p>{{$comic['description']}}</p>
+            </div>
+        </div>
+        <div class="right-content">
+            <!-- pubblicità -->
+            <img src="{{asset('images/spot.jpg')}}" alt="">
+        </div>
+    </div>
+    <div class="bottom-info">
+        <!-- talent info -->
+        <div class="talent">
+            <h2>talent</h2>
+            <!-- art by -->
+            <div class="art-by">
+                <h5>art by</h5>
+                <!-- artists -->
+                <span>
+                     @foreach($comic['artists'] as $artist)
+                        <a href="#">{{$artist}}</a>
+                    @endforeach
+                </span>
+            </div> 
+            <!-- written by -->
+            <div class="written-by">
+                <h5>written by</h5>
+                <!-- writers -->
+                <span>
+                     @foreach($comic['writers'] as $writer)
+                        <a href="#">{{$writer}}</a>
+                    @endforeach
+                </span>
+            </div>   
+        </div>
+        <!-- specs info -->
+        <div class="specs">
+            <h2>specs</h2>
+            <div class="series">
+                <h5>series:</h5>
+                <span>{{$comic['series']}}</span>
+            </div>
+            <div class="us-price">
+                <h5>U.S. Price:</h5>
+                <span>{{$comic['price']}}</span>
+            </div>
+            <div class="on-sale">
+                <h5>On-sale date:</h5>
+                <span>{{$comic['sale_date']}}</span>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
